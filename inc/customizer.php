1e24fa5e5ed1e9612a96851fc9ee10d3532eb4f2
BraydenGray416 <brayden.gray98@gmail.com>
2019-11-28 08:47:15 +1300

    changed text colour in navbar via class

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,17 +14,6 @@ function mytheme_customize_register( $wp_customize ) {
         'settings'   => 'blackSheep_primaryColour',
     ) ) );
 
-    $wp_customize->add_setting( 'blackSheep_primaryTextColour' , array(
-        'default'   => '#FFFFFF',
-        'transport' => 'refresh',
-    ) );
-
-    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'blackSheep_primaryTextColourControl', array(
-        'label'      => __( 'Primary Text Colour', 'blackSheepCustom' ),
-        'description' => 'Change the primary text colour',
-        'section'    => 'colors',
-        'settings'   => 'blackSheep_primaryTextColour',
-    ) ) );
 }
 add_action( 'customize_register', 'mytheme_customize_register' );
 
@@ -34,9 +23,6 @@ function mytheme_customize_css()
     <style type="text/css">
         .primary {
             background-color: <?php echo get_theme_mod('blackSheep_primaryColour', '#000000'); ?>;
-        }
-        .primaryText {
-            color: <?php echo get_theme_mod('blackSheep_primaryTextColour', '#FFFFFF'); ?>;
         }
     </style>
     <?php
